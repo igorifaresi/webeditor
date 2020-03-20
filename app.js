c86f6c826fa1751eed2d8dd30af0a9b4e87c41de
igorifaresi <igor.ifaresi@gmail.com>
2020-03-20 10:42:58 -0300

    More interface behaviour advance

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const templateFile = `
-update = function()
+const templateFile = `update = function()
 
 end
 
@@ -17,19 +16,16 @@ editor.setOptions({
     fontSize: "10pt"
 });
 
-
-//editor.insert(editor.getValue());
-
-var buffers = new Array();
-var actualBuffer = 0;
+var actors = new Array();
+var actualEditingActor = 0;
 
 //tests
 
-addBuffer("foo", "/actors/foo.lua", "a");
-addBuffer("bar", "/actors/bar.lua", "b");
-addBuffer("baz", "/actors/baz.lua", "c");
+addActor("foo", "a");
+addActor("bar", "b");
+addActor("baz", "c");
+addActor("batatinha", templateFile);
 
-//editor.resize();
 
 var socket = io('http://localhost:3000/');
 socket.emit('save', {
@@ -44,44 +40,90 @@ socket.on('load_res', (data) => {
 });
 
 //end tests
+var pages = Array();
+var actualPage = 0;
 
-function addBuffer(_name, _out, _content) {
-    var button = document.createElement("BUTTON");
-    button.setAttribute("onclick", "onTabClick("+buffers.length+")");
-    button.innerHTML = _name;
-    buffers[buffers.length] = {"name" : _name, "tab" : button, "out" : _out,
-                               "content" : _content, "active" : true};
+pages[1] = `
+<div id="lateral-bar">
+    <button onclick="onLateralTabClick(0)">Actors</button>
+    <button onclick="onLateralTabClick(1)">Project</button>
+</div>
+`
 
-    document.getElementById("editor-tabs").innerHTML += button.outerHTML;
+function addActor(name, content) {
+    let tab = document.createElement("BUTTON");
+    tab.setAttribute("onclick", "onEditorTabClick("+actors.length+")");
+    tab.innerHTML = name;
+    let button = document.createElement("div");
+    button.setAttribute("id", "actor-field");
+    button.innerHTML = '<span>'+name+'</span><button onclick="editActor('+actors.length+
+    ')">Edit</button> <button onclick="eraseActor('+actors.length+')">Erase</button>';
+    actors[actors.length] = {"name" : name, "tab" : tab, "button" : button,
+                             "content" : content, "editing" : true};
+
+    document.getElementById("editor-tabs").innerHTML += tab.outerHTML;
+    document.getElementById("actors").innerHTML += button.outerHTML;
 }
 
-function onTabClick(id) {
-    buffers[actualBuffer].content = editor.getValue();
+function editActor(id) {
+    actors[id].editing = true;
+    onEditorTabClick(id);
+    //alert("edit actor: "+id);
+}
 
-    buffers[actualBuffer].tab.setAttribute("id", "tab-inactive");
-    buffers[id].tab.setAttribute("id", "tab-active");
-    var tabs = document.getElementById("editor-tabs");
+function eraseActor(id) {
+    actors.splice(id, 1);
+    let len = actors.length;
+    for (let i = 0; i < len; i++) {
+        actors[i].tab.setAttribute("onclick", "onEditorTabClick("+i+")");
+        actors[i].button.innerHTML = '<span>'+actors[i].name+'</span><button onclick="editActor('+i+
+        ')">Edit</button> <button onclick="eraseActor('+i+')">Erase</button>';
+    }
+    let tabs = document.getElementById("editor-tabs");
+    let actorsList = document.getElementById("actors");
     tabs.innerHTML = "";
-    var len = buffers.length;
-    for (var i = 0; i < len; i++) {
-        if (buffers[i].active) {
-            tabs.innerHTML += buffers[i].tab.outerHTML;
+    actorsList.innerHTML = "";
+    for (let i = 0; i < len; i++) {
+        tabs.innerHTML += actors[i].tab.outerHTML;
+        actorsList.innerHTML += actors[i].button.outerHTML;
+    }
+}
+
+function onEditorTabClick(id) {
+    actors[actualEditingActor].content = editor.getValue();
+
+    actors[actualEditingActor].tab.setAttribute("id", "tab-inactive");
+    actors[id].tab.setAttribute("id", "tab-active");
+    let tabs = document.getElementById("editor-tabs");
+    tabs.innerHTML = "";
+    let len = actors.length;
+    for (let i = 0; i < len; i++) {
+        if (actors[i].editing) {
+            tabs.innerHTML += actors[i].tab.outerHTML;
         }
     }
 
-    editor.setValue(buffers[id].content, -1);
-    actualBuffer = id;
+    editor.setValue(actors[id].content, -1);
+    actualEditingActor = id;
 }
 
-/*
-function getvalue() {
-    return editor.getValue();
+function onLateralTabClick(id) {
+    if (actualPage == 0) {
+        actors[actualEditingActor].content = editor.getValue();
+        editor.destroy();
+    }
+    pages[actualPage] = document.getElementById("main-area").innerHTML;
+    document.getElementById("main-area").innerHTML = pages[id];
+    if (id == 0) {
+        document.getElementById("editor").innerHTML = "";
+        editor = ace.edit("editor");
+        editor.setTheme("ace/theme/ambiance");
+        editor.session.setMode("ace/mode/lua");
+        editor.setOptions({
+            fontFamily: "monospace",
+            fontSize: "10pt"
+        });
+        editor.setValue(actors[actualEditingActor].content, -1);
+    }
+    actualPage = id;
 }
-
-function copyselection() {
-    return editor.getCopyText();
-}
-
-function pastevalue(aValue) {
-    editor.insert(aValue);
-}*/
